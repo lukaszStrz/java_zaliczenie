b3c4ea33d496f08a16eaec636514779fde0b2e5d
Łukasz <lukasz.strz91@gmail.com>
2013-05-31 00:48:01 +0200

    dodawanie książek

--- src/main/java/com/java/java_zaliczenie/App.java
+++ src/main/java/com/java/java_zaliczenie/App.java
@@ -3,6 +3,7 @@ package com.java.java_zaliczenie;
 import com.java.java_zaliczenie.daos.DbDaoFactory;
 import com.java.java_zaliczenie.daos.DaoFactory;
 import com.java.java_zaliczenie.utils.HibernateUtil;
+import java.math.BigDecimal;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -49,13 +50,17 @@ public class App {
                 case 1:
                     task1();
                     break;
-                case 2:
-                    break;
+                case 2: {
+                    Stand stand = selectStand();
+                    Shelf shelf = selectShelf(stand);
+                    addBook(shelf);
+                }
+                break;
                 case 3:
                     break;
                 case 4:
                     break;
-                case 5:
+                case 5: {
                     for (Stand stand : daoFactory.getStandDao().getAllStands()) {
                         System.out.println(stand.getStandName() + ":");
                         for (Object shelf : stand.getShelfs()) {
@@ -65,8 +70,9 @@ public class App {
                             }
                         }
                     }
-                    break;
-                case 6:
+                }
+                break;
+                case 6: {
                     int exportType = 0;
 
                     System.out.println("Wybierz sposób eksportu:");
@@ -90,9 +96,11 @@ public class App {
                         default:
                             break;
                     }
-                    break;
+                }
+                break;
                 case 9:
                     daoFactory = selectDao();
+                    break;
                 default:
                     break;
             }
@@ -200,5 +208,122 @@ public class App {
 
         Shelf shelf = new Shelf(stand, name);
         daoFactory.getShelfDao().addShelf(shelf);
+    }
+
+    private Shelf selectShelf(Stand stand) {
+        System.out.println("Wybierz półkę:");
+        Object[] shelves = stand.getShelfs().toArray();
+        for (int i = 0; i < shelves.length; i++) {
+            System.out.print(i);
+            System.out.println(" - " + ((Shelf) shelves[i]).getShelfName() + "(" + ((Shelf) shelves[i]).getIdShelf().toString() + ")");
+        }
+        do {
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ex) {
+                logger.debug("Choice is not a number");
+                choice = -1;
+                scanner = new Scanner(System.in);
+            }
+            if (choice >= 0 && choice < shelves.length) {
+                return ((Shelf) shelves[choice]);
+            }
+        } while (true);
+    }
+
+    private void addBook(Shelf shelf) {
+        System.out.println("Podaj tytuł:");
+        String title = "";
+        do {
+            title = scanner.nextLine();
+        } while (title.equals(""));
+
+        System.out.println("Podaj autora:");
+        String author = "";
+        do {
+            author = scanner.nextLine();
+        } while (author.equals(""));
+
+        System.out.println("Podaj opis:");
+        String description = "";
+        do {
+            description = scanner.nextLine();
+        } while (description.equals(""));
+
+        System.out.println("Podaj isbn:");
+        String isbn = "";
+        do {
+            isbn = scanner.nextLine();
+            if (isbn.length() != 13 && !isbn.equals("")) {
+                isbn = "";
+                System.out.println("ISBN musi być długości 13 znaków. Podaj ponownie:");
+            }
+        } while (isbn.equals(""));
+
+        System.out.println("Podaj cenę:");
+        BigDecimal price;
+        do {
+            try {
+                price = scanner.nextBigDecimal();
+            } catch (InputMismatchException ex) {
+                logger.debug("Price is not a number");
+                price = BigDecimal.valueOf(-1.0);
+                scanner = new Scanner(System.in);
+            }
+        } while (price.compareTo(BigDecimal.ZERO) == -1);
+
+        Book book = new Book();
+        int choice;
+        do {
+            System.out.println("Kategoria:");
+            System.out.println("1 - dramat");
+            System.out.println("2 - książka przygodowa");
+            System.out.println("3 - SF");
+
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ex) {
+                logger.debug("Choice is not a number");
+                choice = -1;
+                scanner = new Scanner(System.in);
+            }
+
+            switch (choice) {
+                case 1:
+                    book = new DramaBook();
+                    choice = 0;
+                    break;
+                case 2:
+                    System.out.println("Podaj informacje dodatkowe:");
+                    String informations = "";
+                    do {
+                        informations = scanner.nextLine();
+                    } while (informations.equals(""));
+                    book = new AdventureBook();
+                    ((AdventureBook) book).setAdventureInformations(informations);
+                    choice = 0;
+                    break;
+                case 3:
+                    System.out.println("Podaj informacje dodatkowe:");
+                    String movie = "";
+                    do {
+                        movie = scanner.nextLine();
+                    } while (movie.equals(""));
+                    book = new Sfbook();
+                    ((Sfbook) book).setSfMovie(movie);
+                    choice = 0;
+                    break;
+                default:
+                    break;
+            }
+            book.setBookAuthor(author);
+            book.setBookDescription(description);
+            book.setBookIsbn(isbn);
+            book.setBookPrice(price);
+            book.setBookTitle(title);
+            book.setShelf(shelf);
+        } while (choice != 0);
+        daoFactory.getBookDao().addBook(book);
     }
 }
